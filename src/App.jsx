028a8b43e7c48a0d416b548dfd31d00ba1283c62
bharathrnair <bharathrnair7@gmail.com>
2022-03-22 15:28:34 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,114 @@
-import {useState} from "react";
+import blackPawn from './solidpawn.svg';
+import whitePawn from './pawn-regular.svg';
+import rookwhite from './rookwhite.svg';
+import rookblack from './rookblack.svg';
+import knightwhite from './knightwhite.svg'
+import knightblack from './knightblack.svg'
+import bishopwhite from './bishopwhite.svg'
+import bishopblack from './bishopblack.svg'
+import kingwhite   from './kingwhite.svg'
+import kingblack   from './kingblack.svg'
+import queenwhite  from './queenwhite.svg'
+import queenblack from './queenblack.svg'
+
 import './App.css';
 
+
+const initialState = [
+[
+  {
+    icon : rookblack
+  },
+  {
+    icon : knightblack
+  },
+  {
+    icon : bishopblack
+  },
+  {
+    icon : queenblack
+  },
+  {
+    icon : kingblack
+  },
+  {
+    icon : bishopblack
+  },
+  {
+    icon : knightblack
+  },
+  {
+    icon : rookblack
+  },
+],
+[...Array(8)].map(()=>({
+  icon:blackPawn
+}))
+,
+...[...Array(4)].map(()=>[...Array(8)].map(()=>({
+  icon:null
+}))),
+[...Array(8)].map(()=>({
+  icon: whitePawn
+})),
+[
+  {
+    icon : rookwhite
+  },
+  {
+    icon : knightwhite
+  },
+  {
+    icon : bishopwhite
+  },
+  {
+    icon : queenwhite
+  },
+  {
+    icon : kingwhite
+  },
+  {
+    icon : bishopwhite
+  },
+  {
+    icon : knightwhite
+  },
+  {
+    icon : rookwhite
+  },
+]
+]
+
+
+
 const App = () => {
-  const [inputText, setInputText] = useState("");
-  const [todoList, setTodolist] = useState([]);
-  // let count= 0;
   return ( 
-  <div className ="App">
-  <h1>Todo App</h1>
-  <input type="text" value={inputText} onChange={(event)=>{
-  setInputText(event.target.value);
-}}/>
-<button onClick={()=>{
-  setTodolist([...todoList, inputText]);
-  setInputText("");
-}}>Add</button>
-<ul>
-  {todoList.map(todo => (
-    <li>{todo}</li>
-  ))}
-  </ul>    
-  </div>
+  <div className="App">
+    <div className="chess-board-container">
+    <div className="chess-board">
+      {initialState.map((row, i) => row.map(({icon},j) => <div 
+    className='chess-pawn'
+     key={'${i}${j}'}
+     style={
+         {
+
+         backgroundColor:  (i+j)%2? "white" : "brown",
+         color: (i+j)%2 ? "white":"black",
+         backgroundImage: `url(${icon})`,
+         display: "grid",
+         placeItems: "center",
+         }
+
+
+     }
+     
+     
+     
+     ></div>))}
+    </div>
+    </div>
+      </div>
   );
 }
-
 export default App;
+  
